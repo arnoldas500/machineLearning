07b16f86aebad02dc2eda13589574fef3dd1635a
arnoldas500 <akurbanovas@albany.edu>
2017-10-10 08:00:16 -0700

    created pattern array and performacne array

--- forex.py
+++ forex.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 import matplotlib.ticker as mticker
 import matplotlib.dates as mdates
 import numpy as np
+import math
 
 date,bid,ask = np.loadtxt('GBPUSD1d.txt', 
                                 unpack=True, 
@@ -12,10 +13,20 @@ date,bid,ask = np.loadtxt('GBPUSD1d.txt',
                                 converters={0:mdates.bytespdate2num('%Y%m%d%H%M%S')})
     # numpy passes a byte string to the converter instead of a string. In python 2, since bytes and str are equivalent it does not matter. For python 3, however, this results in an error as strpdate2num passes it to the standard library's strptime which only expects a string. (can use slashes too if we have slashes ex 2017/10/30)
 
-def percentChange(startPoint, currPoint):
-    return ((currPoint-startPoint)/startPoint)*100.00
+patternArr= []
+performanceArr = []
+    
+def percentChange(startPoint,currentPoint):
+    try:
+        x = ((float(currentPoint)-startPoint)/abs(startPoint))*100.00
+        if x == 0.0:
+            return 0.000000001
+        else:
+            return x
+    except:
+        return 0.0001
 
-def patternFinder():
+def patternStorage():
     '''
     The goal of patternFinder is to begin collection of %change patterns
     in the tick data. From there, we also collect the short-term outcome
@@ -24,6 +35,7 @@ def patternFinder():
     and even THAT can be machine learned to find the best of all 3 by
     comparing success rates.
     '''
+    patStartTime = time.time()
     
     #Simple Average
     avgLine = ((bid+ask)/2)
@@ -37,6 +49,7 @@ def patternFinder():
     # can be none, buy,
     currentStance = 'none'
     while y < x:
+        pattern = []
         #avgLine[y-10] is our starting point
         #avgLine[y-9] is where we are currently
         p1 = percentChange(avgLine[y-10], avgLine[y-9])
@@ -51,19 +64,45 @@ def patternFinder():
         p10= percentChange(avgLine[y-10], avgLine[y])
 
         outcomeRange = avgLine[y+20:y+30] #array of 10 points
-        currentPoint = avgLine[y]
+        currPoint = avgLine[y]
 
         #function to account for the average of the items in the array
         #lambda used to find a quick way to avg (adds everything together and divs by lenght of outcome range
-        reduce= lambda x, y: x + y, outcomeRange/ len(outcomeRange)
-        print (reduce)
+        try:
+            avgOutCome = reduce= lambda x, y: x + y, outcomeRange/ len(outcomeRange)
+        except Exception as e:
+            print(str(e))
+            avgOutCome = 0
+
+        #print (reduce)
+
+        futureOutCome = percentChange(currPoint, avgOutCome)
+        #pattern.append(p1) is an array of % changes
+        pattern.append(p1)
+        pattern.append(p2)
+        pattern.append(p3)
+        pattern.append(p4)
+        pattern.append(p5)
+        pattern.append(p6)
+        pattern.append(p7)
+        pattern.append(p8)
+        pattern.append(p9)
+        pattern.append(p10)
         
-        print (currentPoint)
-        print ('--------------------------')
-        print (p1, p2, p3, p4, p5, p6, p7, p8, p9, p10)
-        time.sleep(55)
+        patternArr.append(pattern)
+        performanceArr.append(futureOutCome)
+        
+        #print (currPoint)
+        #print ('--------------------------')
+        #print (p1, p2, p3, p4, p5, p6, p7, p8, p9, p10)
+        #time.sleep(55)
         
         y+=1
+
+    patEndTime = time.time()
+    print(len(patternArr))
+    print(len(performanceArr))
+    print('Pattern storage took: ',patEndTime-patStartTime,' seconds')
 
 def graphRawFx():
     
@@ -85,5 +124,5 @@ def graphRawFx():
     plt.grid(True)
     plt.show()
 
-patternFinder()
+patternStorage()
 graphRawFx()
